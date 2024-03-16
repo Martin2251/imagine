14a2b0f00421112876e51d9787709916801e9e8d
Martin Lowe <martinlowe673@gmail.com>
2024-03-16 12:30:16 +0000

    added new sheet

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -36,7 +36,24 @@ const Sidebar = () => {
                             )
                         })}
                         </ul>
-                    <ul>
+                    <ul className='sidebar-nav_elements'>
+                    {navLinks.slice(6).map((link) =>{
+                            const isActive = link.route === pathname
+                            return(
+                                <li key={link.route} className={`sidebar-nav_element group ${isActive? 'bg-purple-gradient text-white': 'text-gray-700'}`}>
+                                    <Link className="sidebar-link" href={link.route}>
+                                        <Image 
+                                        src={link.icon}
+                                        width={24}
+                                        height={24}
+                                        className={`${isActive && 'brightness-200'}`}
+                                        alt="logo"
+                                        />
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            )
+                        })}
                         <li className='flex-center cursor-pointer gap-2 p-4'>
                         <UserButton afterSignOutUrl='/' showName />
                         </li>
