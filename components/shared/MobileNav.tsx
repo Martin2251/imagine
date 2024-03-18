b9c1e6bd00aee31af0c46d05cf595336e4667a31
Martin Lowe <martinlowe673@gmail.com>
2024-03-18 20:26:34 +0000

    mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
 
 const MobileNav = () => {
   return (
-    <div>
-      
-    </div>
+    <header className='header'>
+
+    </header>
   )
 }
 
