5e7f8c9dcd1f113789ba61a841320cce59bdd63c
Martin Lowe <martinlowe673@gmail.com>
2024-03-19 08:00:44 +0000

    closing tag

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -31,7 +31,7 @@ const MobileNav = () => {
               <SheetTrigger><Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className='cursor-pointer' /></SheetTrigger>
               <SheetContent className='sheet-content sm:w-64'>
                 <>
-                <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23}
+                <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23}  />
                 </>
               </SheetContent>
             </Sheet>
