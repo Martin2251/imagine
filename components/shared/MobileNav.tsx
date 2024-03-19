65c73edfda87718e04c97f02f43c24ae555eeed9
Martin Lowe <martinlowe673@gmail.com>
2024-03-19 08:00:26 +0000

    mobile nav

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -29,14 +29,10 @@ const MobileNav = () => {
           <UserButton afterSignOutUrl='/'  />
             <Sheet>
               <SheetTrigger><Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className='cursor-pointer' /></SheetTrigger>
-              <SheetContent>
-                <SheetHeader>
-                  <SheetTitle>Are you absolutely sure?</SheetTitle>
-                  <SheetDescription>
-                    This action cannot be undone. This will permanently delete your account
-                    and remove your data from our servers.
-                  </SheetDescription>
-                </SheetHeader>
+              <SheetContent className='sheet-content sm:w-64'>
+                <>
+                <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23}
+                </>
               </SheetContent>
             </Sheet>
 
