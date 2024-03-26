7a160acf6cc0c1f43e5923a13f8bd70f9cf9badf
Martin Lowe <martinlowe673@gmail.com>
2024-03-26 15:01:27 +0000

    use client on mobile

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,18 +1,20 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import {
   Sheet,
   SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
+
   SheetTrigger,
 } from "@/components/ui/sheet"
 import Link from 'next/link'
 import { SignedIn, UserButton } from '@clerk/nextjs'
+import { navLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
 
 
 const MobileNav = () => {
+  const pathname = usePathname()
   return (
     <header className='header'>
       <Link href="/" className='flex items-center gap-2 md:py-2'>
@@ -32,6 +34,25 @@ const MobileNav = () => {
               <SheetContent className='sheet-content sm:w-64'>
                 <>
                 <Image src="/assets/images/logo-text.svg" alt="logo" width={152} height={23}  />
+                <ul className='header-nav_elements'>
+                        {navLinks.map((link) =>{
+                            const isActive = link.route === pathname
+                            return(
+                                <li key={link.route} className={`sidebar-nav_element group ${isActive? 'bg-purple-gradient text-white': 'text-gray-700'}`}>
+                                    <Link className="sidebar-link" href={link.route}>
+                                        <Image 
+                                        src={link.icon}
+                                        width={24}
+                                        height={24}
+                                        className={`${isActive && 'brightness-200'}`}
+                                        alt="logo"
+                                        />
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            )
+                        })}
+                        </ul>
                 </>
               </SheetContent>
             </Sheet>
