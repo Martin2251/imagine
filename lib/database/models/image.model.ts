603e641fa13332349aadf9592080f0f989f22028
Martin Lowe <martinlowe673@gmail.com>
2024-03-29 14:51:39 +0000

    image model

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const ImageSchema = new Schema({
     title:{type:String, required:true},
@@ -12,6 +12,13 @@ const ImageSchema = new Schema({
     aspectRatio:{type:String},
     color:{type:String},
     prompt:{type:String},
+    author:{type:Schema.Types.ObjectId, ref:'User'},
+    createdAt:{type:Date, default: Date.now},
+    updatedAt:{type:Date, default: Date.now}
 
     
 })
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image
