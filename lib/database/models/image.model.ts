93dedfbe578c8e4902ed6224ae8ae5bea08c40cb
Martin Lowe <martinlowe673@gmail.com>
2024-03-29 14:53:38 +0000

    added interface

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,5 +1,27 @@
 import { Schema, model, models } from "mongoose";
 
+interface Image {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: URL;
+    width?: number;
+    height?: number;
+    config?: object;
+    transformationUrl?: URL;
+    aspectRatio?: string;
+    color?: string;
+    prompt?: string;
+    author: Schema.Types.ObjectId | string; // Assuming Schema.Types.ObjectId is imported from Mongoose
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+// Assuming the Schema.Types.ObjectId and URL types are imported from Mongoose
+// If not, you need to define them in your code or use alternatives
+
+
+
 const ImageSchema = new Schema({
     title:{type:String, required:true},
     transformationType:{type:String, required:true},
